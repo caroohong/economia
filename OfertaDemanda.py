63f9bbfef159b055ae6045c61df6829164b3432e
Carolina <80550684+caroohong@users.noreply.github.com>
2022-04-16 17:23:17 -0400

    Update OfertaDemanda.py

--- OfertaDemanda.py
+++ OfertaDemanda.py
@@ -1,19 +1,20 @@
 #Pd= DAPmax - d*Qd
 #Pd: precio pagado por el consumidor
-#DAPmax: maxima dispocision a pagar del consumidor
+#DAPmax: maxima dispocision a pagar del consumidor: 13.99
 #d: pendiente de la demanda
 #Qd: cantidad que el consumidor quiere comprar para su determinado precio
 
 #Ps = COmin + s*Qs
 #Ps: precio percibido por el productor
-#Comin: costo de oportunidad minimo (interseccion con eje y) 
+#Comin: costo de oportunidad minimo del productor(interseccion con eje y) 
 #s: pemdiente oferta
 #Qs: cantidad que el productor quiere proveer
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib inline
 class mercado:
-
     def __init__(self, DAPmax, COmin, d, s):
         self.DAPmax, self.COmin, self.d, self.s = DAPmax, COmin, d, s
-        
         if DAPmax < COmin:
             raise ValueError('Demanda insuficiente.')
 
@@ -44,3 +45,21 @@ class mercado:
     def oferta(self,x):
         "Función oferta"
         return self.COmin + self.s*x
+#DAPmax, COmin, d, s
+param_i = 13.99,0,1,5
+m = mercado(*param_i) 
+
+q_max = m.cantidad_equilibrio() * 2
+q_grid = np.linspace(0.0, q_max, 100)
+pd = m.demanda(q_grid)
+ps = m.oferta(q_grid)
+
+fig, ax = plt.subplots()
+ax.plot(q_grid, pd, lw=2, alpha=0.6, label='demanda')
+ax.plot(q_grid, ps, lw=2, alpha=0.6, label='oferta')
+ax.set_xlabel('cantidad', fontsize=14)
+ax.set_xlim(0, q_max)
+ax.set_ylabel('precio', fontsize=14)
+ax.legend(loc='lower right', frameon=False, fontsize=14)
+ax.set(title='Oferta, Demanda y Equilibrio de mercados')
+plt.show()
