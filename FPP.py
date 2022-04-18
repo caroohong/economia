31ffb629e24e9602585a7f8b52f6b9a77ba8bd8a
Carolina <80550684+caroohong@users.noreply.github.com>
2022-04-17 22:00:05 -0400

    grafico FPP
    Plataformas de streaming y cines
    'si voy al cine, dejo de usar la plataforma de streaming y viceversa'
    Bibliograia:
    - codigo de google colaboratory
    - datos del servicio de impuestos internos: Produccion peliculas cinematograficas, Exhibicion peliculas cinematograficas

--- FPP.py
+++ FPP.py
@@ -2,4 +2,79 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from ipywidgets import interact, fixed
+#parametros iniciales
+ALPHA = 0.2   # capital share in agriculture
+BETA = 0.5   # capital share in manufacturing
+KBAR = 265  #Straming
+LBAR = 44  #Cines
+p = 1      # =Pa/Pm  relative price of ag goods
+#pendiente: LBAR/KBAR
+#FUNCIONES BIEN F, BIEN G
+def F(K,L,alpha=ALPHA):
+    """Agriculture Production function"""
+    return (K**alpha)*(L**(1-alpha))
 
+def G(K,L,beta=BETA):
+    """Manufacturing Production function"""
+    return (K**beta)*(L**(1-beta))
+#BUDGET
+def budgetc(c1, p1, p2, I):
+    return (I/p2)-(p1/p2)*c1
+
+def isoq(L, Q, mu):
+    return (Q/(L**(1-mu)))**(1/mu)
+
+def edgeworth(L, Kbar=KBAR, Lbar=LBAR,alpha=ALPHA, beta=BETA):
+    """efficiency locus: """
+    a = (1-alpha)/alpha
+    b = (1-beta)/beta
+    return b*L*Kbar/(a*(Lbar-L)+b*L)
+
+def edgeplot(LA, Kbar=KBAR, Lbar=LBAR,alpha=ALPHA,beta=BETA):
+    """Draw an edgeworth box
+    
+    arguments:
+    LA -- labor allocated to ag, from which calculate QA(Ka(La),La) 
+    """
+    KA = edgeworth(LA, Kbar, Lbar,alpha, beta)
+    RTS = (alpha/(1-alpha))*(KA/LA)
+    QA = F(KA,LA,alpha)
+    QM = G(Kbar-KA,Lbar-LA,beta)
+    print("(LA,KA)=({:4.1f}, {:4.1f})  (QA, QM)=({:4.1f}, {:4.1f})  RTS={:4.1f}"
+          .format(LA,KA,QA,QM,RTS))
+    La = np.arange(1,Lbar)
+    fig, ax = plt.subplots(figsize=(7,6))
+    ax.set_xlim(0, Lbar)
+    ax.set_ylim(0, Kbar)
+    ax.plot(La, edgeworth(La,Kbar,Lbar,alpha,beta),'k-')
+    #ax.plot(La, La,'k--')
+    ax.plot(La, isoq(La, QA, alpha))
+    ax.plot(La, Kbar-isoq(Lbar-La, QM, beta),'g-')
+    ax.plot(LA, KA,'ob')
+    ax.vlines(LA,0,KA, linestyles="dashed")
+    ax.hlines(KA,0,LA, linestyles="dashed")
+    ax.text(-6,-6,r'$O_A$',fontsize=16)
+    ax.text(Lbar,Kbar,r'$O_M$',fontsize=16)
+    ax.set_xlabel(r'$L_A -- Labor$', fontsize=16)
+    ax.set_ylabel('$K_A - Capital$', fontsize=16)
+    #plt.show()
+
+def fpp(LA,Kbar=KBAR, Lbar=LBAR,alpha=ALPHA,beta=BETA):
+    KA = edgeworth(LA, Kbar, Lbar,alpha, beta)
+    RTS = (alpha/(1-alpha))*(KA/LA)
+    QA = F( KA,LA,alpha)
+    QM = G(Kbar-KA,Lbar-LA,beta)
+    ax.scatter(QA,QM)
+    La = np.arange(0,Lbar)
+    Ka = edgeworth(La, Kbar, Lbar,alpha, beta)
+    Qa = F(Ka,La,alpha)
+    Qm = G(Kbar-Ka,Lbar-La,beta)
+    ax.set_xlim(0, Lbar)
+    ax.set_ylim(0, Kbar)
+    ax.plot(Qa, Qm,'k-')
+    ax.set_xlabel('Plataformas de Streaming',fontsize=18)
+    ax.set_ylabel('Cines',fontsize=18)
+    plt.show()
+
+fig, ax = plt.subplots(figsize=(7,6))
+fpp(309,alpha=0.2,beta=0.5)
